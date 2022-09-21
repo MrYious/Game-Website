b00d6cd3bb66ff3529f9a8c82bac3a48f09329c1
MrYious <rosariomark37@gmail.com>
2022-09-21 11:24:52 +0800

    feat: install & setup of tailwind, autoprefixer, postcss

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        @vite('resources/css/app.css')
 
         <title>Laravel</title>
 
@@ -20,7 +21,7 @@
             }
         </style>
     </head>
-    <body class="antialiased">
+    <body>
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
