161a569766f18e9987285b44c33185b7dfc5e11f
MrYious <rosariomark37@gmail.com>
2022-09-23 12:02:38 +0800

    feat: added template page file

---
+++ resources/views/template.blade.php
@@ -0,0 +1,19 @@
+<!-- DUPLICATE THIS TEMPLATE FOR NEW PAGES -->
+
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        @vite('resources/css/app.css')
+        <title>ChromaHunt</title>
+        @livewireStyles
+    </head>
+    <body>
+        <!-- INSERT YOUR CODE HERE -->
+
+        <script>
+        </script>
+        @livewireScripts
+    </body>
+</html>
