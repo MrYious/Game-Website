34826bc7e657c0f23ba23aac512bf32be9435bbd
jessieissej <jakadusale@iskolarngbayan.pup.edu.ph>
2022-09-28 15:15:46 +0800

    fix: minor UI design fix and spacing

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -17,26 +17,27 @@
             <div>
                 Login
             </div>
-            <form class="flex flex-col w-full p-6 h-fit bg-slate-500">
+            <!-- Login box -->
+            <form class="flex flex-col w-full p-6 h-fit bg-slate-500 rounded-lg">
                 <div class="flex flex-col items-start text-[28px] ">
                     <div class="flex flex-row gap-3 p-3">
                         <svg xmlns="http://www.w3.org/2000/svg" class="w-10 h-10" viewBox="0 0 20 20" fill="currentColor">
                             <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
                             <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
                         </svg>
-                        <input class="" name="email" type="email" placeholder="Email*" required/>
+                        <input class="font-semibold" name="email" type="email" placeholder="Email*" required/>
                     </div>
                     <div class="flex flex-row gap-3 p-3">
                         <svg xmlns="http://www.w3.org/2000/svg" class="w-10 h-10" viewBox="0 0 20 20" fill="currentColor">
                             <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd"></path>
                         </svg>
-                        <input minlength={6} maxlength="16" name="password" type="password" placeholder="Password*" required/>
+                        <input class="font-semibold" minlength={6} maxlength="16" name="password" type="password" placeholder="Password*" required/>
                     </div>
                 </div>
                 <button type="submit" class="flex justify-center w-full p-2 text-xl font-bold tracking-wide text-gray-200 bg-green-900 rounded-sm shadow-md shadow-black">
                 LOGIN
                 </button>
-                <div class="flex text-[20px] pt-3 justify-center">
+                <div class="flex text-[20px] pt-3 gap-1 justify-center">
                     <span>No account yet? </span>
                     <a href="register" class="text-red-500 font"> Register here. </a>
                 </div>
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -17,40 +17,41 @@
             <div>
                 Register
             </div>
-            <form class="flex flex-col w-full p-6 h-fit bg-slate-500">
+            <!-- Register box -->
+            <form class="flex flex-col w-full p-6 h-fit bg-slate-500 rounded-lg">
                 <div class="flex flex-col items-start text-[28px] ">
                     <div class="flex flex-row gap-3 p-3">
                         <svg xmlns="http://www.w3.org/2000/svg" class="w-10 h-10" viewBox="0 0 20 20" fill="currentColor">
                             <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path>
                         </svg>
-                        <input maxlength="30" name="name" type="text" placeholder="Name*" required/>
+                        <input class="font-semibold" maxlength="30" name="name" type="text" placeholder="Name*" required/>
                     </div>
                     <div class="flex flex-row gap-3 p-3">
                         <svg xmlns="http://www.w3.org/2000/svg" class="w-10 h-10" viewBox="0 0 20 20" fill="currentColor">
                             <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
                             <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
                         </svg>
-                        <input class="" name="email" type="email" placeholder="Email*" required/>
+                        <input class="font-semibold" name="email" type="email" placeholder="Email*" required/>
                     </div>
                     <div class="flex flex-row gap-3 p-3">
                         <svg xmlns="http://www.w3.org/2000/svg" class="w-10 h-10" viewBox="0 0 20 20" fill="currentColor">
                             <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd"></path>
                         </svg>
-                        <input minlength={6} maxlength="16" name="password" type="password" placeholder="Password*" required/>
+                        <input class="font-semibold" minlength={6} maxlength="16" name="password" type="password" placeholder="Password*" required/>
                     </div>
                     <div class="flex flex-row gap-3 p-3">
                         <svg xmlns="http://www.w3.org/2000/svg" class="w-10 h-10" viewBox="0 0 20 20" fill="currentColor">
                             <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd"></path>
                         </svg>
-                        <input minlength={6} maxlength="16" name="confirmPassword" type="password" placeholder="Confirm Password*" required/>
+                        <input class="font-semibold" minlength={6} maxlength="16" name="confirmPassword" type="password" placeholder="Confirm Password*" required/>
                     </div>
                 </div>
                 <button type="submit" class="flex justify-center w-full p-2 text-xl font-bold tracking-wide text-gray-200 bg-green-900 rounded-sm shadow-md shadow-black">
                 REGISTER
                 </button>
-                <div class="flex text-[20px] pt-3 justify-center">
+                <div class="flex text-[20px] pt-3 gap-1 justify-center">
                     <span>Already have an account?</span>
-                    <a href="login" class="gap-2 text-red-500 font"> Login here. </a>
+                    <a href="login" class=" text-red-500 font"> Login here. </a>
                 </div>
             </form>
             </div>
