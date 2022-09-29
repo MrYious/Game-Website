cc512ca05838e76ccf4478e71a4378eb61f00354
MiguelLiwanag <86527599+MiguelLiwanag@users.noreply.github.com>
2022-09-29 15:50:38 +0800

    feat: added validation for login and registration

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Session;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -12,5 +16,46 @@ class AuthController extends Controller
 
     public function register(){
         return view('register');
+    }
+
+    function registeruser(Request $request)
+    {
+        $request->validate([
+            'name'              =>   'required',
+            'email'             =>   'email:rfc,dns|required|email|unique:users',
+            'password'          =>   'required|min:6',
+            'confirmPassword'   =>   'required|min:6'
+        ]);
+
+        $data = $request->all();
+
+        if($data['password'] == $data['confirmPassword']){
+            User::create([
+                'name'  =>  $data['name'],
+                'email' =>  $data['email'],
+                'password' => Hash::make($data['password'])
+            ]);
+
+            return redirect('login')->with('message', 'Registration Successful');
+        }
+
+        return redirect('register')->with('message', 'Passwords does not match');
+    }
+
+    function loginuser(Request $request)
+    {
+        $request->validate([
+            'email'         =>  'required',
+            'password'      =>  'required'
+        ]);
+
+        $credentials = $request->only('email', 'password');
+
+        if(Auth::attempt($credentials))
+        {
+            return redirect('');
+        }
+
+        return redirect('login')->with('message', 'Incorrect email or password');
     }
 }
