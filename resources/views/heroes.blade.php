73af34f6f8ee225f3ccea61c20821015b3847943
LaelHub <93648850+LaelHub@users.noreply.github.com>
2022-09-30 15:16:37 +0800

    feat: minor changes
    -heroes page minor chages

--- resources/views/heroes.blade.php
+++ resources/views/heroes.blade.php
@@ -30,15 +30,15 @@
         <!-- NAVBAR -->
         <livewire:navbar />
         <!-- HEROES -->
-        <!-- IMAGES -->
         <section id="heroes" class="flex items-center justify-center w-full min-h-screen gap-10 bg-slate-300">
             <!-- IMAGE GROUPS -->
             <div class="flex justify-start w-[60%] h-[550px] border-2 border-black ">
+                <!-- IMAGE 1 -->
                 <div class="flex items-start w-1/4 ">
-                    <div class="flex items-center justify-center w-[120%] -m-5  h-4/5 border-[1px] border-black" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">Hero Image 1</div>
+                    <div class="flex items-center justify-center w-[120%] -m-5  h-4/5 border-[1px] border-black" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" role="button" aria-controls="offcanvasExample">Hero Image 1</div>
                 </div>
                 <!-- POP UP -->
-                <div class="offcanvas offcanvas-start fixed bottom-0 flex flex-col max-w-full bg-white invisible shadow-sm outline-none transition duration-300 ease-in-out text-gray-700 top-0 left-0 border-none w-200" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+                <div class="offcanvas offcanvas-start fixed bottom-0 flex flex-col max-w-full bg-white invisible shadow-sm outline-none transition duration-300 ease-in-out text-gray-700 top-0 left-0 border-none w-96" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
                     <div class="offcanvas-header flex items-center justify-between p-4">
                         <h5 class="offcanvas-title mb-0 leading-normal font-semibold" id="offcanvasExampleLabel">Hero 1</h5>
                         <button type="button" data-bs-dismiss="offcanvas" aria-label="Close">
@@ -48,12 +48,65 @@
                         </button>
                     </div>
                     <div class="offcanvas-body flex-grow p-4 overflow-y-auto">
-                        <div>
-                        Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.
-                        </div>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe dignissimos nesciunt maiores voluptatem eius ipsum at natus labore veniam eum, dolorum corrupti a minima ab fugit. Quisquam modi sequi pariatur minus placeat. Temporibus sapiente aut soluta, blanditiis ipsam sint exercitationem sit, doloremque rem iure, quaerat qui quisquam ab corporis minima.
+                    </div>
+                </div>
+                <!-- IMAGE 2 -->
+                <div class="flex items-end w-1/4 ">
+                    <div class="flex items-center justify-center w-[120%] -m-5  h-4/5 border-[1px] border-black" data-bs-toggle="offcanvas" data-bs-target="#offcanvasTop" role="button" aria-controls="offcanvasTop">Hero Image 2</div>
+                </div>
+                <!-- POP UP -->
+                <div class="offcanvas offcanvas-start fixed bottom-0 flex flex-col max-w-full bg-white invisible shadow-sm outline-none transition duration-300 ease-in-out text-gray-700 top-0 left-0 border-none h-96" tabindex="-1" id="offcanvasTop" aria-labelledby="offcanvasTopLabel">
+                    <div class="offcanvas-header flex items-center justify-between p-4">
+                        <h5 class="offcanvas-title mb-0 leading-normal font-semibold" id="offcanvasTopLabel">Hero 2</h5>
+                        <button type="button" data-bs-dismiss="offcanvas" aria-label="Close">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                            </svg>
+                        </button>
+                    </div>
+                    <div class="offcanvas-body flex-grow p-4 overflow-y-auto">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe dignissimos nesciunt maiores voluptatem eius ipsum at natus labore veniam eum, dolorum corrupti a minima ab fugit. Quisquam modi sequi pariatur minus placeat. Temporibus sapiente aut soluta, blanditiis ipsam sint exercitationem sit, doloremque rem iure, quaerat qui quisquam ab corporis minima.
+                    </div>
+                </div>
+                <!-- IMAGE 3 -->
+                <div class="flex items-start w-1/4 ">
+                    <div class="flex items-center justify-center w-[120%] -m-5  h-4/5 border-[1px] border-black" data-bs-toggle="offcanvas" data-bs-target="#offcanvasBottom" role="button" aria-controls="offcanvasBottom">Hero Image 3</div>
+                </div>
+                <!-- POP UP -->
+                <div class="offcanvas offcanvas-bottom fixed bottom-0 flex flex-col max-w-full bg-white invisible bg-clip-padding shadow-sm outline-none transition duration-300 ease-in-out text-gray-700 left-0 right-0 border-none h-1/3 max-h-full" tabindex="-1" id="offcanvasBottom" aria-labelledby="offcanvasBottomLabel">
+                    <div class="offcanvas-header flex items-center justify-between p-4">
+                        <h5 class="offcanvas-title mb-0 leading-normal font-semibold" id="offcanvasBottomLabel">Hero 3</h5>
+                        <button type="button" data-bs-dismiss="offcanvas" aria-label="Close">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                            </svg>
+                        </button>
+                    </div>
+                    <div class="offcanvas-body flex-grow p-4 overflow-y-auto small">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Est culpa aliquid voluptate ad, esse consequuntur illo odio eveniet dicta consectetur iure quis ipsa quod reprehenderit perferendis ducimus. Ratione dolores distinctio blanditiis in molestias repellat ipsa debitis, dolorum, quibusdam expedita libero delectus nostrum voluptas. Sunt recusandae impedit autem accusantium perspiciatis mollitia!
+                    </div>
+                </div>
+                <!-- IMAGE 4 -->
+                <div class="flex items-end w-1/4 ">
+                    <div class="flex items-center justify-center w-[120%] -m-5  h-4/5 border-[1px] border-black" data-bs-toggle="offcanvas"  data-bs-target="#offcanvasRight" role="button" aria-controls="offcanvasRight">Hero Image 4</div>
+                </div>
+                <!-- POP UP -->
+                <div class="offcanvas offcanvas-end fixed bottom-0 flex flex-col max-w-full bg-white invisible bg-clip-padding shadow-sm outline-none transition duration-300 ease-in-out text-gray-700 top-0 right-0 border-none w-96" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+                    <div class="offcanvas-header flex items-center justify-between p-4">
+                        <h5 class="offcanvas-title mb-0 leading-normal font-semibold" id="offcanvasRightLabel">Hero 4</h5>
+                        <button type="button" data-bs-dismiss="offcanvas" aria-label="Close">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                            </svg>
+                        </button>
+                    </div>
+                    <div class="offcanvas-body flex-grow p-4 overflow-y-auto">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur explicabo autem perferendis qui ex, odit nemo id temporibus aliquam possimus excepturi! Cupiditate nesciunt voluptatum consequatur ex explicabo, ducimus magni adipisci dolor facilis. Ut expedita sed odit, quam atque error tenetur tempora itaque quia, suscipit dolorum veritatis praesentium accusantium quas deleniti.
                     </div>
                 </div>
             </div>
+            
         </section>
 
 
