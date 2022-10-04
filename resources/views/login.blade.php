d70c6922912c739c1f812d04af6ffdbe7b67a8cc
jessieissej <jakadusale@iskolarngbayan.pup.edu.ph>
2022-10-04 16:03:21 +0800

    fix: minor placeholder fix

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -31,7 +31,7 @@
                             <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
                             <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
                         </svg>
-                        <input class="font-semibold" name="email" type="email" placeholder="Email*" required/>
+                        <input class="font-semibold " name="email" type="email" placeholder="email@domain.com" required/>
                     </div>
                     <div class="flex flex-row gap-3 p-3">
                         <svg xmlns="http://www.w3.org/2000/svg" class="w-10 h-10" viewBox="0 0 20 20" fill="currentColor">
